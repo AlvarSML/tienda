617dce9bc78507e21a4404d1e3bf79eba0eb45c4
alvarsnow <alvarsnow@gmail.com>
2017-12-10 21:07:32 +0100

    sa

---
+++ script.js
@@ -0,0 +1,17 @@
+let menu = true; 
+
+window.onload = f => {
+  document.getElementById("boton").addEventListener("click",fMenu);
+}
+
+function fMenu() {
+  let side = document.getElementById("menuSide");
+
+  if (menu) {
+    menu = !menu;
+    side.style.display = "none";
+  } else {
+    menu = !menu;
+    side.style.display = "block";
+  }
+}
